62f23c1d0cdb8c1571fcb0cbd7c53bb1039d8227
Yokiyami <130506092+Yokiyami@users.noreply.github.com>
2023-05-28 22:12:23 +0200

    login en vluchtzoekfunctie werkend gemaakt.

---
+++ applicatie/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bruno+Ace&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>Gelre Checkin</title>
+</head>
+
+<body>
+    <header>
+        <img src="img/logo.png" alt="Een plaatje van een vliegtuigje als een logo">
+        <h1> Gelre Checkin</h1>
+        <div class="login-button">
+            <form action="functies.php" method="GET">
+                <select name="login-als" onchange="this.form.submit()">
+                    <option disabled selected>Log in als</option>
+                    <option value="passagier-login.php">Passagier</option>
+                    <option value="medewerker-login.php">Medewerker</option>
+                </select>
+            </form>
+        </div>
+    </header>
+    <nav class="desktop-nav">
+        <ul>
+            <a href="index.php">Startpagina</a>
+            <a href="privacyverklaring.php">Privacyverklaring</a>
+            <a href="contact.php">Contact</a>
+            <a href="vluchtenoverzicht.php">Vluchtenoverzicht</a>
+        </ul>
+    </nav>
+    <nav class="mobile-nav">
+        <form action="functies.php" method="GET">
+            <select name="pagina" onchange="this.form.submit()">
+                <option disabled selected>Menu</option>
+                <option value="index.php">Startpagina</option>
+                <option value="privacyverklaring.php">Privacyverklaring</option>
+                <option value="contact.php">Contact</option>
+                <option value="vluchtenoverzicht.php">Vluchtenoverzicht</option>
+            </select>
+        </form>
+    </nav>
+    <div class="main-container">
